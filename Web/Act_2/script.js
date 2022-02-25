2b48c85daff790ac102a13a0afabb51ee63b494e
JoseTobon <a01411655@itesm.mx>
2022-02-25 00:11:09 -0600

    Act2

---
+++ Web/Act_2/script.js
@@ -0,0 +1,4 @@
+function formcitaa () {
+    alerta1 = "DATA SUBMITTED:" + "\n\n"+ document.getElementById("name").value + "\n" + document.getElementById("discID").value + "\n" + document.getElementById("steamID").value + "\n\n" + "THANKS! :D";
+    alert(alerta1);
+}
