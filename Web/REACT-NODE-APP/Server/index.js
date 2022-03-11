961cf472cd1cd8119f4bb958fa4747c885a75d31
JoseTobon <a01411655@itesm.mx>
2022-03-11 08:37:32 -0600

    Endpoints

--- Web/REACT-NODE-APP/Server/index.js
+++ Web/REACT-NODE-APP/Server/index.js
@@ -1,15 +1,32 @@
 // server/index.js
 
 const express = require("express");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
-
 app.get("/api", (req, res) => {
     res.json({message: "Hello from server side!" });
+});
+
+app.get("/pets", (req, res) => {
+    fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
+        console.log( data );
+        res.end( data );
+    });
+});
+
+app.post("/addPet", (req, res) => {
+    fs.readFile( __dirname + "/" + "pets.json", "utf8", (req, res) => {
+        data = JSON.parse( data );
+        data["pet4"] = newPet["pet4"];
+        console.log( data );
+        res.end( JSON.stringify( data ) );
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
 });
