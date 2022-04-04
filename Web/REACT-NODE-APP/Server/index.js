efe26c2a2c0e7f4b667d19c67a228edbcdf8f7d8
JoseTobon <a01411655@itesm.mx>
2022-04-03 23:28:39 -0500

    Enpoints

--- Web/REACT-NODE-APP/Server/index.js
+++ Web/REACT-NODE-APP/Server/index.js
@@ -17,6 +17,18 @@ const newPet = {
     }
 }
 
+const newPet2 = {
+    "pet3" : {
+        "name" : "kream",
+        "type" : "dog",
+        "owner" : "tobs",
+        "color" : "yorkie color",
+        "id" : 3
+    }
+}
+
+var id = 1;
+
 app.get("/api", (req, res) => {
     res.json({message: "Hello from server side!" });
 });
@@ -28,10 +40,51 @@ app.get("/pets", (req, res) => {
     });
 });
 
+
 app.post("/addPet", (req, res) => {
-    fs.readFile( __dirname + "/" + "pets.json", "utf8", (req, res) => {
+    fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
+        data = JSON.parse( data );
+        data["pet3"] = newPet2["pet3"];
+        fs.writeFile(__dirname + "/" + "pets.json", JSON.stringify( data ), (err) => {
+            if (err) throw err;
+        });
+        console.log( data );
+        res.end( JSON.stringify( data ) );
+    });
+});
+
+app.post("/addPet2", (req, res) => {
+    fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
         data = JSON.parse( data );
         data["pet4"] = newPet["pet4"];
+        fs.writeFile(__dirname + "/" + "pets.json", JSON.stringify( data ), (err) => {
+            if (err) throw err;
+        });
+        console.log( data );
+        res.end( JSON.stringify( data ) );
+    });
+});
+
+app.put("/modifyPet1" , (req, res) => {
+    fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
+        data = JSON.parse( data );
+        newPet.pet4["id"] = 5;
+        data["pet4"] = newPet["pet4"];
+        fs.writeFile(__dirname + "/" + "pets.json", JSON.stringify( data ), (err) => {
+            if (err) throw err;
+        });
+        console.log( data );
+        res.end( JSON.stringify( data ) );
+    });
+})
+
+app.delete("/deletePet", (req, res) => {
+    fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
+        data = JSON.parse( data );
+        delete data["pet1"];
+        fs.writeFile(__dirname + "/" + "pets.json", JSON.stringify( data ), (err) => {
+            if (err) throw err;
+        });
         console.log( data );
         res.end( JSON.stringify( data ) );
     });
